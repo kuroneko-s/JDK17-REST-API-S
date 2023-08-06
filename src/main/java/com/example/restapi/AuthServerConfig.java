bde0da704b475efb446e2432a970232e7bd11329
kuroneko <drivespublic@gmail.com>
2023-08-07 08:49:50 +0900

    OAuth2.0 Server 구성중...

--- src/main/java/com/example/restapi/AuthServerConfig.java
+++ src/main/java/com/example/restapi/AuthServerConfig.java
@@ -1,7 +1,56 @@
 package com.example.restapi;
 
+import com.example.restapi.account.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
-public class AuthServerConfig {
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+        security.passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        super.configure(clients);
+        clients.inMemory() // in memory로 등록
+                .withClient("testApp") // client id 설정
+                .authorizedGrantTypes("password", "refresh_token") // 인증 후 token값을 주는데 어떤 값들을 추가로 받을지 설정
+                .scopes("임의의 값") // 임의의 값 ?
+                .secret(this.passwordEncoder.encode("aa1234")) // 패스워드
+                .accessTokenValiditySeconds(60 * 10)
+                .refreshTokenValiditySeconds(60 * 10 * 6);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(accountService)
+                .tokenStore(tokenStore);
+    }
 }
--- src/test/java/com/example/restapi/auth/AuthServerConfigTest.java
+++ src/test/java/com/example/restapi/auth/AuthServerConfigTest.java
@@ -1,15 +1,24 @@
 package com.example.restapi.auth;
 
+import com.example.restapi.account.domain.Account;
+import com.example.restapi.account.domain.AccountRole;
 import com.example.restapi.account.service.AccountService;
 import com.example.restapi.event.contorller.AbstractControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
+import org.springframework.security.web.AuthenticationEntryPoint;import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -22,7 +31,16 @@ class AuthServerConfigTest extends AbstractControllerTest {
 
     @Test
     @DisplayName("인증 토큰을 발급 받는 테스트")
-    public void getAuthToken() throws Exception{
+    public void getAuthToken() throws Exception {
+        String username = "test@test.com";
+        String password = "aa1234";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountService.saveAccount(account);
+
         // 토큰을 발급받는 스프링 Auth2.0 제공하는 6가지 방법
         // Grant Type - password
         // 한번의 요청으로 토큰 발급 가능.
@@ -31,11 +49,22 @@ class AuthServerConfigTest extends AbstractControllerTest {
         String clientSecret = "pass";
 
         this.mockMvc.perform(post("/oauth/token")
-                        .with(httpBasic(clientId, clientSecret))) // BASIC_AUTH Header 생성
+                        .with(httpBasic(clientId, clientSecret)) // BASIC_AUTH Header 생성
+                        .param("username", username)
+                        .param("password", password)
+                        .param("grant_type", "password")
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("access_token").exists())
         ;
+
+        // BearerTokenAuthenticationToken
+//         SecurityContextRepository
+//        DelegatingSecurityContextRepository
+//        HttpSessionSecurityContextRepository
+        // AuthenticationEntryPoint
+
     }
 
 
