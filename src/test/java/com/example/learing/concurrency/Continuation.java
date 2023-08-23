9277dbae936ac4c33077bfb7ebd685bddade444d
kuroneko <drivespublic@gmail.com>
2023-08-23 22:26:57 +0900

    Spring MVC 5.7.x 작업 시작.

---
+++ src/test/java/com/example/learing/concurrency/Continuation.java
@@ -0,0 +1,11 @@
+package com.example.learing.concurrency;
+
+import ch.qos.logback.core.util.SystemInfo;
+
+public class Continuation implements Runnable {
+
+
+    @Override
+    public void run() {
+    }
+}
